253d2969fcaef28db0e4518cb4c856a31e10743c
tonypera <paulurbian@hotmail.com>
2019-02-11 13:33:24 -0500

    styles form and config cloudinary server

--- src/app.js
+++ src/app.js
@@ -2,27 +2,27 @@ const express = require('express');
 const morgan = require('morgan');
 const multer = require('multer');
 const path = require('path');
-const exhbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
 
-//Initializations
+// Initializations
 const app = express();
 require('./database');
 
-//Settings
+// Settings
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.engine('.hbs', exhbs({
+app.engine('.hbs', exphbs({
     defaultLayout: 'main',
-    layoutsDir: path.join(app.get('views', 'layouts')),
-    partialsDir: path.join(app.get('views', 'partials')),
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
     extname: '.hbs'
-}));
+}))
 app.set('view engine', '.hbs');
 
-// Midlewares
+// Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({extended: false}));
 const storage = multer.diskStorage({
     destination: path.join(__dirname, 'public/uploads'),
     filename: (req, file, cb) => {
@@ -34,8 +34,4 @@ app.use(multer({storage}).single('image'));
 // Routes
 app.use(require('./routes'));
 
-// Static Files
-app.use(express.static(path.join(__dirname, 'public')));
-
-//exports app --> index.js
 module.exports = app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,38 @@
-const express = require('express');
-const router = express.Router();
+const { Router } = require('express');
+const router = Router();
 
-router.get('/', (req ,res) => {
-    res.send('hola');
+const Photo = require('../models/Image');
+
+const cloudinary = require('cloudinary');
+const fs = require('fs-extra');
+// Config module cloudinary
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
+router.get('/', (req ,res) => {
+   res.render('images')
+});
+
+router.get('/images/add', (req, res) => {
+    res.render('form-img')
+});
+
+router.post('/images/add', async (req, res) => {
+    const { title, description } = req.body;
+    const result = await cloudinary.v2.uploader.upload(req.file.path);
+    console.log(result);
+    const newPhoto = new Photo({
+        title,
+        description,
+        imageURL: result.url,
+        public_id: result.public_id
+    });
+    await newPhoto.save();
+    await fs.unlink(req.file.path);
+    res.send('received');
+});
+
 
 module.exports = router;
